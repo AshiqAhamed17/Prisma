55b5f124289eef505d922b6d23d3a63ff8bfb307
Ashiq Ahamed <aashiq121205@gmail.com>
2024-12-07 21:23:05 +0530

    prisma schema

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const app = express();
+const client = new PrismaClient();
+
+
+async function createUser() {
+    await client.user.create ({
+        data: {
+            id: 123,
+            username: "Ashiq",
+            password: "123123",
+            age: 18
+        }
+    })
+}
+
+createUser();
